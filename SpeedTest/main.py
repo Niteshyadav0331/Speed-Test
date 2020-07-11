2f57d5afd2db93885f6ff4551c5580f5585238de
Nitesh Yadav <55007875+Niteshyadav178@users.noreply.github.com>
2020-07-11 16:41:27 +0530

    Add files via upload

---
+++ SpeedTest/main.py
@@ -0,0 +1,14 @@
+"""
+
+You can check your internet speed just by typing some commands of python 
+No need to download any speed test apk 
+
+all we have to do is install speed test module of python in command prompt by using ::
+>>>pip install speedtest-cli
+
+then run the command in command prompt ::
+>>>speedtest-cli
+
+This will show the speed of your internet
+
+"""
